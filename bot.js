7632a03c0162cbc85fbc5d73342460d51765e3f9
kolton256 <63267483+kolton256@users.noreply.github.com>
2020-04-30 23:21:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 
 // Events.
 bot.on("ready", function() {
-    bot.user.setGame(`Hi, Im New Bot On Discord!`);
+    bot.user.setGame(`Working at DG`);
     console.log(`${bot.user.username} is Ready!`);
 });
 
